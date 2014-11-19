b0d1258dd576e332816330b3b67d88515dfcba65
alexpchin <alex@womapp.com>
2014-11-19 16:04:43 +0000

    Made relative path using pathname

--- lib/url-resolver.rb
+++ lib/url-resolver.rb
@@ -1,4 +1,5 @@
 require "url-resolver/version"
+require "pathname"
 
 module UrlResolver
   
@@ -10,7 +11,10 @@ module UrlResolver
       if path_to_resolve.include?("//")
         "http:#{path_to_resolve}"
       else 
-        "#{url}#{path_to_resolve}"
+        first = Pathname.new(url)
+        second = Pathname.new(path_to_resolve)
+        relative = second.relative_path_from first
+        first + relative
       end
     else
       path_to_resolve
